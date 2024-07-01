a400bd320aa712eaaca4f339e6f1e151f4cf7cbf
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-07-01 17:08:12 -0300

    feat/ Started and created the mix project

---
+++ elixir_moeda/lib/elixir_moeda.ex
@@ -0,0 +1,18 @@
+defmodule ElixirMoeda do
+  @moduledoc """
+  Documentation for `ElixirMoeda`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirMoeda.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir_moeda/test/elixir_moeda_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirMoedaTest do
+  use ExUnit.Case
+  doctest ElixirMoeda
+
+  test "greets the world" do
+    assert ElixirMoeda.hello() == :world
+  end
+end
