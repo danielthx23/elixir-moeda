808491ce5784158f6d85cc25d2903ea7944aaa9c
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-07-01 17:19:59 -0300

    docs/ Created header and main description of the project

--- elixir_moeda/lib/elixir_moeda.ex
+++ elixir_moeda/lib/elixir_moeda.ex
@@ -1,18 +1,16 @@
 defmodule ElixirMoeda do
   @moduledoc """
-  Documentation for `ElixirMoeda`.
+  Documentação do módulo Moeda
+  Versão: 1.1.1
+  Criado por Daniel Akiyama
+  GitHub: https://github.com/danielthx23
+  Linkedin: https://www.linkedin.com/in/daniel-akiyama/
+
+  Desafio proposto pela Stone: https://gist.github.com/Isabelarrodrigues/873b8849e8b54f0968d77a4b2f111ec4
+
+  Este módulo serve para reunir várias funções de operações financeiras.
+  Tanto as mais básicas, como subtrair e somar, quanto operações complexas como a de Split e de conversão de moedas.
+  Este aplicativo utiliza uma estrutura de dados abstrata para representar valores monetários em centavos e suas respectivas moedas.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirMoeda.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
